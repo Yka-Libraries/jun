8b37ee1c1449fdccb9079add94aea2bfffca240a
杨少 <samyangcoder@gmail.com>
2021-11-07 01:38:10 +0800

    add dom structure

---
+++ src/dom.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+
+type AttrMap = HashMap<String, String>;
+
+pub struct Node {
+    /// children of a dom node
+    children: Vec<Node>,
+
+    /// type of a dom node
+    node_type: NodeType,
+}
+
+enum NodeType {
+    /// text node
+    Text(String),
+
+    /// element node
+    Element(ElementData),
+}
+
+struct ElementData {
+    /// tag name of element node,
+    /// like `div`、`p`
+    tag_name: String,
+
+    /// attributes of element node, is a map like `<name, value>`
+    attributes: AttrMap,
+}
+
+impl Node {
+    /// create text node
+    fn text(data: String) -> Node {
+        // text node has no children
+        Node {
+            children: Vec::new(),
+            node_type: NodeType::Text(data),
+        }
+    }
+
+    /// create element node
+    fn element(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+        Node {
+            children,
+            node_type: NodeType::Element(ElementData {
+                tag_name: name,
+                attributes: attrs,
+            }),
+        }
+    }
+}
