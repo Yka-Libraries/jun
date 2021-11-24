52dd09b8b72d7cb302f3717f9845448849aaa4ff
杨少 <samyangcoder@gmail.com>
2021-11-25 00:55:58 +0800

    feat: update css parser & add specificity

--- src/css.rs
+++ src/css.rs
@@ -11,9 +11,52 @@ struct Rule {
     declarations: Vec<Declaration>,
 }
 
+/// **Specificity** is one of the ways a rendering engine decides which style
+/// overrides the other in a conflict. If a stylesheet contains two rules
+/// that match an element, the rule with the matching selector of higher
+/// specificity can override values from the one with lower specificity.
+/// 
+/// # Examples
+/// 
+/// ```
+///  *             {}  /* a=0 b=0 c=0 d=0 -> specificity = 0,0,0,0 */
+///  li            {}  /* a=0 b=0 c=0 d=1 -> specificity = 0,0,0,1 */
+///  li:first-line {}  /* a=0 b=0 c=0 d=2 -> specificity = 0,0,0,2 */
+///  ul li         {}  /* a=0 b=0 c=0 d=2 -> specificity = 0,0,0,2 */
+///  ul ol+li      {}  /* a=0 b=0 c=0 d=3 -> specificity = 0,0,0,3 */
+///  h1 + *[rel=up]{}  /* a=0 b=0 c=1 d=1 -> specificity = 0,0,1,1 */
+///  ul ol li.red  {}  /* a=0 b=0 c=1 d=3 -> specificity = 0,0,1,3 */
+///  li.red.level  {}  /* a=0 b=0 c=2 d=1 -> specificity = 0,0,2,1 */
+///  #x34y         {}  /* a=0 b=1 c=0 d=0 -> specificity = 0,1,0,0 */
+///  style=""          /* a=1 b=0 c=0 d=0 -> specificity = 1,0,0,0 */
+/// <HEAD>
+/// <STYLE type="text/css">
+///   #x97z { color: red }
+/// </STYLE>
+/// </HEAD>
+/// <BODY>
+/// <P ID=x97z style="color: green">
+/// </BODY>
+/// ```
+/// 
+/// above example is from `https://www.w3.org/TR/CSS2/cascade.html#specificity`,
+/// but here for simple purpose, **We use three number to decide the specificity.**
+/// 
+pub type Specificity = (usize, usize, usize);
+
 /// selector of css, like a tag name、a class name prefixed by '.'、'*'
 enum Selector {
     Simple(SimpleSelector),
+}
+
+impl Selector {
+    pub fn specificity(&self) -> Specificity {
+      let Selector::Simple(ref simple) = *self;
+      let a = simple.id.iter().count();
+      let b = simple.class.len();
+      let c = simple.tag_name.iter().count();
+      (a, b, c)
+    }
 }
 
 /// simple selector without any combinator
@@ -51,6 +94,10 @@ struct Color {
     a: u8,
 }
 
+fn valid_identifier_char(identifier: char) -> bool {
+  todo!()
+}
+
 struct Parser {}
 
 impl Parser {
@@ -62,6 +109,42 @@ impl Parser {
             class: Vec::new(),
         };
 
-        todo!()
+        while !self.eof() {
+          match self.next_char() {
+              '#' => {
+                self.consume_char();
+                selector.id = Some(self.parse_identifier());
+              },
+              '.' => {
+                self.consume_char();
+                selector.class.push(self.parse_identifier());
+              },
+              '*' => {
+                self.consume_char();
+              },
+              c if valid_identifier_char(c) => {
+                selector.tag_name = Some(self.parse_identifier());
+              },
+              _ => break
+          }
+        }
+
+        return selector
+    }
+
+    fn eof(&mut self) -> bool {
+      todo!()
+    }
+
+    fn next_char(&mut self) -> char {
+      todo!()
+    }
+
+    fn consume_char(&mut self) -> char {
+      todo!()
+    }
+
+    fn parse_identifier(&mut self) -> String {
+      todo!()
     }
 }
